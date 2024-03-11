2389cd41b7e303f720948cc5596ad19c35c0f50a
Sima Raifen <simar@skyvar.co.il>
2024-03-11 14:39:45 +0200

    :memo: check what is working

--- slack/conversation.js
+++ slack/conversation.js
@@ -14,10 +14,8 @@ export default class Conversation {
     
     async conversations() {
         try {
-            console.log("efi");
             const result = await this.client.conversations.list();
-            //this.getConversationsId(result.channels);
-            console.log(result.channels);
+            this.getConversationsId(result.channels);
         }
         catch (error) {
             console.error(error);
